b1cb03c1b5d3ee93d9e8bc4c123149ab0c63a4f6
mauricefhebert <mauricefhebert@gmail.com>
2022-03-22 01:21:42 -0400

    Add function to create a grid off any size

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const gridContainer = document.querySelector('.container');
+
+function createGrid(row, cell) {
+  for (let i = 0; i < row; i++) {
+    const x = document.createElement('div');
+    x.classList.add('grid-item');
+    gridContainer.append(x);
+    for (let j = 0; j < cell; j++) {
+      const y = document.createElement('div');
+      y.classList.add('grid-item');
+      x.append(y);
+    }
+  }
+}
+
+createGrid(16, 16);
