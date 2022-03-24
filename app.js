93546ee3c43d78e331b5cf2278e03f1b4d02f922
mauricefhebert <mauricefhebert@gmail.com>
2022-03-23 21:58:42 -0400

    add some button for the setting

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const gridContainer = document.querySelector('.container');
+const gridCell = document.querySelectorAll('.grid-cell');
+let color = document.querySelector('#color');
 
 function createGrid(row, cell) {
   for (let i = 0; i < row; i++) {
@@ -8,9 +10,12 @@ function createGrid(row, cell) {
     for (let j = 0; j < cell; j++) {
       const y = document.createElement('div');
       y.classList.add('grid-item');
+      y.classList.add('grid-cell');
       x.append(y);
     }
   }
 }
-
 createGrid(16, 16);
+gridContainer.addEventListener('mouseover', (e) => {
+  e.target.closest('.grid-cell').style.backgroundColor = color.value;
+});
